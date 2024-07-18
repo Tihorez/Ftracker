5715db071e0379e40b40e1641320f9e13a637879
Tikhon Svintsitskiy <dragonbal-3@protonmail.ch>
2024-07-18 22:59:27 +0300

    Change WalkingSpentCalories

--- ftracker.go
+++ ftracker.go
@@ -105,7 +105,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
     // ваш код здесь
-    ...
+    speed := meanSpeed(action, duration) * kmhInMsec
+	return ((walkingCaloriesWeightMultiplier * weight + (math.Pow(speed, 2) / (height / cmInM)) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
